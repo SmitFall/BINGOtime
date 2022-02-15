e5637eab162453011b7a3b4241616d6b1225f698
Smitfall <captainfalconrsmith@gmail.com>
2022-02-15 08:53:57 -0700

    Add project files.

---
+++ BINGOtime/BINGOtime.vb
@@ -0,0 +1,11 @@
+﻿Module BINGOtime
+    'TODO how many balls for each letter
+    'TODO draw a random ball letter - B - I - N - G - O - with numbers
+    'TODO track what balls have come up in an array
+    'TODO clear trackin history on new game
+    'TODO don't draw duplicates
+    Sub Main()
+
+    End Sub
+
+End Module
